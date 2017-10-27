aa10f4d7fd01174652f31c6606fe94b65b1a4a4a
Vlad <vlad165432@gmail.com>
2017-10-27 09:25:40 +0300

    add State_machine struct

--- src/lexanalyzer.cpp
+++ src/lexanalyzer.cpp
@@ -2,6 +2,9 @@
 #include <iostream>
 #include <map>
 #include <ctype.h>
+#include <string>
+#include <list>
+#include <unordered_set>
 #include "lexanalyzer.h"
 
 using namespace std;
@@ -11,8 +14,8 @@ int analyze(char* fname)
     ifstream fin;
     fin.open (fname);
     char ch;
-
-
+    init_state_machines();
+    init_hash_table();
     while (fin >> noskipws >> ch) {
         Symbol smb(ch);
         recognize(smb);
@@ -23,37 +26,133 @@ int analyze(char* fname)
     return 0;
 }
 
+
+void init_state_machines()
+{
+    init_main_machine();
+    init_number_machine();
+}
+
+State_machine main_machine(9,8);
+void init_main_machine()
+{
+    main_machine.add_branch(empty_st, digit, number_st,number_act);
+    main_machine.add_branch(empty_st, letter, word_st,nil);
+    main_machine.add_branch(empty_st, comment, empty_st,nil);
+    main_machine.add_branch(empty_st, comparison, comparison_st,nil);
+    main_machine.add_branch(empty_st, logic, logic_st,nil);
+    main_machine.add_branch(empty_st, math, math_st,nil);
+    main_machine.add_branch(empty_st, separator, separator_st,nil);
+    main_machine.add_branch(empty_st, wrong_symb, empty_st,nil);
+    main_machine.add_branch(number_st, digit, number_st,number_act);
+    main_machine.add_branch(number_st, letter, number_st,number_act);
+    main_machine.add_branch(word_st, digit, word_st,nil);
+    main_machine.add_branch(word_st, letter, word_st,nil);
+    main_machine.add_branch(comparison_st, comparison, comparison_st,nil);
+    main_machine.add_branch(logic_st, logic, logic_st,nil);
+    main_machine.add_branch(math_st, math, math_st,nil);
+}
+
+State_machine number_machine(8,5);
+void init_number_machine()
+{
+    number_machine.add_branch(empty_nst, minus_num, minus_nst,nil);
+    number_machine.add_branch(empty_nst, digit_num, int_nst,nil);
+    number_machine.add_branch(empty_nst, dot_num, dot_nst,nil);
+    number_machine.add_branch(minus_nst, digit_num, int_nst,nil);
+    number_machine.add_branch(minus_nst, dot_num, dot_nst,nil);
+    number_machine.add_branch(int_nst, digit_num, int_nst,nil);
+    number_machine.add_branch(int_nst, dot_num, dot_nst,nil);
+    number_machine.add_branch(dot_nst, digit_num, fraction_nst,nil);
+    number_machine.add_branch(fraction_nst, digit_num, fraction_nst,nil);
+    number_machine.add_branch(fraction_nst, e_num, e_nst,nil);
+    number_machine.add_branch(e_nst, plus_num, sign_nst,nil);
+    number_machine.add_branch(e_nst, minus_num, sign_nst,nil);
+    number_machine.add_branch(sign_nst, digit_num, order_nst,nil);
+}
+
+
+
 int recognize(Symbol& smb)
 {
-    static char transitions[7][8] = {
-    //   digit     letter   comment  comparison    logic    math    separator    wrong_symb
-        {number_st,word_st, empty_st,comparison_st,logic_st,math_st,separator_st,wrong_st}, // empty_st
-        {number_st,number_st,end_st,  end_st,      end_st,  end_st, end_st,      wrong_st}, // number_st
-        {word_st,  word_st, end_st,   end_st,      end_st,  end_st, end_st,      wrong_st}, // word_st
-        {end_st,   end_st,  end_st,  comparison_st,end_st,  end_st, end_st,      wrong_st}, // comparison_st
-        {end_st,   end_st,  end_st,   end_st,      logic_st,end_st, end_st,      wrong_st}, // logic_st
-        {end_st,   end_st,  end_st,   end_st,      end_st,  math_st,end_st,      wrong_st}, // math_st
-        {end_st,   end_st,  end_st,   end_st,      end_st,  end_st, end_st,      wrong_st} // separator_st
-    };
-    static map<char,map<char,pair<char,char>>> = {
-        {digit,map<char,pair<char,char>>(empty_st,pair<char,char>(number_st,0))};
-    }
     static char current_state = empty_st;
     static string val;
-    current_state = transitions[current_state][smb.type];
-    cout << (int)current_state;
-    if(current_state < end_st) {
-        val+=smb.ch;
-    } else if(current_state == end_st) {
-        cout << val <<endl;
-        val.clear();
-        current_state = empty_st;
-        recognize(smb);
-    } else {
-        cerr << "wrong symbol" <<endl;
+    State_act  state_act = main_machine.transitions[current_state][smb.type];
+
+
+    switch(state_act.act) {
+        case number_act: {
+
+        }
+        break;
+        case end_act: {
+
+            categorize(val,current_state);
+
+            val.clear();
+            current_state = state_act.next;
+            recognize(smb);
+            return 0;
+        }
+        break;
     }
+    val+=smb.ch;
+    current_state = state_act.next;
 
+    return 0;
+}
 
+void categorize(string str, char state)
+{
+    switch(state) {
+        case word_st: {
+            int key = find_key_word(str);
+            if(key>0) {
+                cout<<"keyword "<<str<<' '<<key<<endl;
+            }
+        }
+        break;
+        case separator_st: {
+            if(str == " " || str == "\n") {
+                return;
+            }
+        }
+    }
+    cout<<str <<" "<< (int)state <<endl;
+}
+
+list<Keyword> key_words[10];
+hash<string> hash_fn;
+void init_hash_table()
+{
+    add_hash("if",if_key);
+    add_hash("else",else_key);
+    add_hash("for",for_key);
+    add_hash("while",while_key);
+    add_hash("break",break_key);
+    add_hash("continue",continue_key);
+    add_hash("bool",while_key);
+    add_hash("int",break_key);
+    add_hash("double",continue_key);
+    add_hash("true",true_key);
+    add_hash("false",false_key);
+}
+
+void add_hash(string word, Keys key)
+{
+    key_words[hash_fn(word)%10].push_back(Keyword(word,key));
+}
+
+int find_key_word(string word)
+{
+    int h = hash_fn(word)%10;
+    auto it = key_words[h].begin();
+    while (it!= key_words[h].end()) {
+        if(it->word == word) {
+            return it->key;
+        }
+        it++;
+    }
     return 0;
 }
 
@@ -87,4 +186,41 @@ Symbol::Symbol(char c) {
     } else {
         type = defenition.find(ch)->second;
     }
+}
+
+State_act::State_act(){
+   this->next = empty_st;
+   this->act = end_act;
+}
+
+State_act::State_act(int next, Act act){
+   this->next = next;
+   this->act = act;
+}
+
+Keyword::Keyword(string word, Keys key)
+{
+    this->word = word;
+    this->key = key;
+}
+
+State_machine::State_machine(int state_num,int smb_num)
+{
+    this->state_num = state_num;
+    this->smb_num = smb_num;
+    transitions = new State_act*[state_num];
+    for(int i=0;i<state_num;i++)
+        transitions[i] = new State_act[smb_num];
+}
+
+void State_machine::add_branch(int old_state, int smb, int next_state, Act call_back)
+{
+    transitions[old_state][smb] = State_act(next_state,call_back);
+}
+
+State_machine::~State_machine()
+{
+    for(int i=0;i<state_num;i++)
+        delete transitions[i];
+    delete transitions;
 }
--- src/lexanalyzer.h
+++ src/lexanalyzer.h
@@ -1,6 +1,7 @@
 #ifndef LEXANALYZER_H
 #define LEXANALYZER_H
-enum Symbol_type{
+#include <string>
+enum Symbol_type {
     digit, // 0..9
     letter, // a..z _
     comment, // / *
@@ -10,24 +11,108 @@ enum Symbol_type{
     separator, // , ; ( ) { } \n
     wrong_symb
 };
-enum States{
+enum States {
+    end_st,
+    wrong_st,
     empty_st,
     number_st,
     word_st,
     comparison_st, // == != > <
     logic_st, // && ||
     math_st, // ++ -- + - = !
-    separator_st,
-    end_st,
-    wrong_st
+    separator_st
 };
+
+enum Number_symbol_type {
+    digit_num,
+    dot_num,
+    e_num,
+    plus_num,
+    minus_num
+};
+
+enum Number_states {
+    empty_nst,
+    minus_nst,
+    dot_nst,
+    int_nst,
+    fraction_nst,
+    e_nst,
+    sign_nst,
+    order_nst
+};
+
+enum Act{
+    nil,
+    number_act,
+    end_act
+};
+
+enum Keys {
+    if_key = 1,
+    else_key,
+    for_key,
+    while_key,
+    break_key,
+    continue_key,
+    bool_key,
+    int_key,
+    double_key,
+    true_key,
+    false_key
+};
+
+enum Lex_class {
+    id_class,
+    int_class,
+    float_class,
+    bool_class,
+    logic_class
+};
+
+struct Keyword {
+    Keyword(std::string word, Keys key);
+    std::string word;
+    Keys key;
+};
+
 struct Symbol {
     Symbol(char c);
     char ch;
     Symbol_type type;
 };
 
+struct State_act {
+    State_act();
+    State_act(int next, Act act);
+    int next;
+    Act act;
+};
+
+struct Lexem {
+    std::string name;
+
+};
+
+struct State_machine {
+    State_machine(int state_num,int smb_num);
+    void add_branch(int old_state, int smb, int next_state, Act call_back);
+    ~State_machine();
+    int state_num;
+    int smb_num;
+    int current_state = 0;
+    State_act** transitions;
+};
+
+
 
 int analyze(char* fname);
 int recognize(Symbol& smb);
+void init_state_machines();
+void init_main_machine();
+void init_number_machine();
+void init_hash_table();
+void add_hash(std::string word, Keys key);
+void categorize(std::string str, char state);
+int find_key_word(std::string word);
 #endif
